3fd544400ccca3b825431b62db3505cb82819f7d
Hiroaki Nakamura <hnakamur@gmail.com>
2015-06-15 17:14:21 +0900

    Simplify file_list example

--- cmd/file_list/main.go
+++ cmd/file_list/main.go
@@ -3,29 +3,20 @@ package main
 import (
 	"flag"
 	"fmt"
-	"log"
 	"os"
 	"strings"
 
 	"github.com/hnakamur/garoonclient"
 )
 
-var logFileName string
+var folderID string
 
 func init() {
-	flag.StringVar(&logFileName, "logfilename", "file_list.log", "log file name")
+	flag.StringVar(&folderID, "folderid", "", "folder ID")
 }
 
 func main() {
 	flag.Parse()
-
-	logFile, err := os.Create(logFileName)
-	if err != nil {
-		panic(err)
-	}
-	defer logFile.Close()
-	log.SetOutput(logFile)
-	log.SetFlags(log.LstdFlags)
 
 	baseURL := os.Getenv("BASE_URL")
 	username := os.Getenv("USERNAME")
@@ -38,52 +29,22 @@ func main() {
 		Expires:  "2037-08-12T14:45:00Z",
 		Locale:   "jp",
 	}
-	folderListReq := garoonclient.CabinetGetFolderInfoRequest{Header: header}
-	folderListRes, err := garoonclient.CabinetGetFolderInfo(&folderListReq)
+	fileListReq := garoonclient.CabinetGetFileInfoRequest{Header: header, FolderID: folderID}
+	fileListRes, err := garoonclient.CabinetGetFileInfo(&fileListReq)
 	if err != nil {
 		panic(err)
 	}
-	_, err = fmt.Println(strings.Join(
-		[]string{
-			"hid",
-			"location",
-			"file_id",
-			"title",
-			"filename",
-			"modify_time",
-		},
-		"\t"))
-	if err != nil {
-		panic(err)
-	}
-	walkFn := func(folder *garoonclient.CabinetFolder, parent *garoonclient.CabinetFolder) error {
-		fileListReq := garoonclient.CabinetGetFileInfoRequest{Header: header, FolderID: folder.ID}
-		fileListRes, err := garoonclient.CabinetGetFileInfo(&fileListReq)
+	for _, file := range fileListRes.Files {
+		_, err = fmt.Println(strings.Join(
+			[]string{
+				file.ID,
+				file.Title,
+				file.Name,
+				file.ModifyTime,
+			},
+			"\t"))
 		if err != nil {
-			if err == garoonclient.ResponseTagNotFoundError {
-				log.Printf("GetFileInfo response tag not found. hid=%s", folder.ID)
-				// continue
-				return nil
-			}
-			return err
+			panic(err)
 		}
-		for _, file := range fileListRes.Files {
-			_, err = fmt.Println(strings.Join(
-				[]string{
-					folder.ID,
-					strings.Join(folder.Location, " > "),
-					file.ID,
-					file.Title,
-					file.Name,
-					file.ModifyTime,
-				},
-				"\t"))
-			return err
-		}
-		return nil
-	}
-	err = folderListRes.Root.Walk(walkFn, nil)
-	if err != nil {
-		panic(err)
 	}
 }
